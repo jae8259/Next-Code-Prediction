9181782129b9ca22f9d6497cc7645b21b561d956
jae8259 <dragonj5290@gmail.com>
2026-02-26 14:53:43 +0900

    add samples

---
+++ samples/test_code.py
@@ -0,0 +1,18 @@
+
+def greet(name, age):
+    """
+    This function greets the user.
+    """
+    message = f"Hello, {name}! You are {age} years old."
+    print(message)
+
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+def calculate_sum(a, b):
+    # This function calculates sum of two numbers
+    return a + b
